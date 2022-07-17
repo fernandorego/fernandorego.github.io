729739f50b626f330ed523380549b95e709fc0e0
Fernando Rego <fernandoluisrego@gmail.com>
2022-07-18 00:02:17 +0100

    Add fun banner

--- src/elements.ts
+++ src/elements.ts
@@ -7,11 +7,22 @@ export class Banner {
     }
 
     setText(): void {
-        let text: HTMLParagraphElement = document.createElement('p');
-        text.innerText = 'Hello traveller! Welcome to my web terminal.\n\
-                            Type help to discover all the possibilities at your disposal\n';
+        let banner_welcome: HTMLParagraphElement = document.createElement('p');
+        banner_welcome.innerHTML  = '                                                                                ____            \n';
+        banner_welcome.innerHTML += '                                                                               | |  \\_______    \n';
+        banner_welcome.innerHTML += '██      ██  ██████ ██       ████   ████   ████  ████ ██████  ██                | |          \\   \n';
+        banner_welcome.innerHTML += '██      ██  ██     ██     ██     ██    ██ ██  ██  ██ ██      ██         O    __|_|___________|  \n';
+        banner_welcome.innerHTML += '██      ██  ████   ██     ██     ██    ██ ██      ██ ████    ██        /\\_  /____|____________) \n';
+        banner_welcome.innerHTML += '██  ██  ██  ██     ██     ██     ██    ██ ██      ██ ██               /   |___________________| \n';
+        banner_welcome.innerHTML += '  ██  ██    ██████ ██████   ████   ████   ██      ██ ██████  ██      /__    | )(            )(  \n';
+        banner_welcome.innerHTML += '                                                                     | |\\  :| )(            )(  \n';
 
-        this.banner.appendChild(text);
+        let banner_text: HTMLParagraphElement = document.createElement('p');
+        banner_text.innerHTML = 'Hi! My name is Fernando Rego and this is my web terminal.\n';
+        banner_text.innerHTML += 'Type help to display the list of all available commands';
+
+        this.banner.appendChild(banner_welcome);
+        this.banner.appendChild(banner_text);
     }
 
     getBanner(): HTMLDivElement {
